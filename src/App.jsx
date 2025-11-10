62d977464c3b0fbc3b50af0eb047bf78eb307c1d
Brianna-Lilly <briannalilly247@gmail.com>
2025-11-10 14:44:59 -0500

    Frontend + navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar from './components/navbar/navbar'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/assets.js
@@ -0,0 +1,9 @@
+import logo from './logo.png'
+import basketIcon from './basket_icon.png'
+import searchIcon from './search_icon.png'
+
+export const assets = {
+    logo,
+    basketIcon,
+    searchIcon
+}
