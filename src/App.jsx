476681d9210d00f1ef2f015869c9b3357ad1c173
KristaUSingh <109554138+KristaUSingh@users.noreply.github.com>
2025-11-10 14:56:52 -0500

    fix homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import navbar from './components/navbar/navbar'
+import Navbar from "./components/navbar/navbar"
 
-const App = () => {
+function App() {
   return (
-    <div className='app'>
-      <navbar/>
+    <div className="app">
+      <Navbar />
     </div>
   )
 }
