fb9bd3a76b529fd5acc188f5965696a7ea6ddbb7
KristaUSingh <109554138+KristaUSingh@users.noreply.github.com>
2025-11-28 19:34:35 -0500

    connected storecontext and supabase connection

--- src/components/navbar/FoodDisplay/FoodDisplay.jsx
+++ src/components/navbar/FoodDisplay/FoodDisplay.jsx
@@ -1,25 +1,26 @@
-import React, {useContext} from 'react'
-import './FoodDisplay.css'
-import { StoreContext } from '../../../context/StoreContext'
+import React, { useContext } from "react";
+import "./FoodDisplay.css";
+import { StoreContext } from "../../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
+const FoodDisplay = ({ category }) => {
+  const { food_list } = useContext(StoreContext);
 
-const FoodDisplay = ({category}) => {
-  
-    const {food_list} = useContext(StoreContext)
-  
-    return (
-    <div className = 'food-display' id='food-display'>
+  return (
+    <div className="food-display" id="food-display">
       <h2>Top Dishes Near You</h2>
+
       <div className="food-display-list">
-        {food_list.map((item,index)=>{
-            if(category==="All" || category===item.category){
-                return <FoodItem key={index} id={item._id} name={item.name} price={item.price} image={item.image}/> 
+        {food_list &&
+          food_list.map((item, index) => {
+            if (category === "All" || category === item.category) {
+              return <FoodItem key={index} item={item} />;
             }
-        })}
+            return null;
+          })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FoodDisplay
+export default FoodDisplay;
