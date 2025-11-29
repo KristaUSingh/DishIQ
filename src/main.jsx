b65b147f457572c2be11810807a20d91fafdc1fa
linnnegan <71103693+linnnegan@users.noreply.github.com>
2025-11-29 03:32:56 -0500

    Router configuration and context addition

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,64 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter, 
+  RouterProvider,
+} from 'react-router-dom' 
+
+// Navbar
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import StoreContextProvider from "./context/StoreContext.jsx";
-import { AuthProvider } from "./context/AuthContext";
 
+// Pages
+import HomePage from './pages/Home/Home.jsx'
+import RestaurantsPage from './pages/RestaurantPage/RestaurantsPage.jsx'
+import ContactPage from './pages/ContactUs/ContactPage.jsx'
+import LoginPage from './pages/Login/Login.jsx'
+import RegistrationPage from './pages/Signup/Signup.jsx'
+
+// Context Providers
+import StoreContextProvider from "./context/StoreContext.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";  // <-- ADD THIS
+
+// Core router def w paths
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />, // App is the Layout component (Navbar, Outlet)
+    children: [
+      {
+        index: true, 
+        element: <HomePage />, 
+      },
+      {
+        path: 'restaurants', 
+        element: <RestaurantsPage />,
+      },
+      {
+        path: 'contact', 
+        element: <ContactPage />,
+      },
+      {
+        path: 'login', 
+        element: <LoginPage />,
+      },
+      {
+        path: 'register', 
+        element: <RegistrationPage />,
+      },
+    ],
+  },
+  // Protected routes (e.g., /chef, /manager) need to add
+]);
+
+// Render app
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>                     {/* <-- WRAP APP IN AUTH */}
-        <StoreContextProvider>
-          <App />
-        </StoreContextProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider> 
+      <StoreContextProvider>
+        {/* FIX: Render the router object using RouterProvider */}
+        <RouterProvider router={router} />
+      </StoreContextProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
